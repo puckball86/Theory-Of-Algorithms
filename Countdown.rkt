c12c344987b2ea39b607af9f660af70c7335d797
dstarr86 <starrdara@gmail.com>
2017-04-22 22:06:06 +0100

    Using example to try implement a stack

--- Countdown.rkt
+++ Countdown.rkt
@@ -7,6 +7,18 @@ trgtNum
 
 (define start (list -1 -1 -1 -1 1 1 1 1))
 
+;Stack implementation: Push and pop items from the list 
+;reference: http://stackoverflow.com/questions/29244677/implementation-of-lifo-list-in-scheme
+(define (make-stack)
+  (let ((stack '()))
+    (lambda (msg . args)
+      (cond 
+        [(eq? msg 'pop!)  (set! stack (cdr stack))]
+        [(eq? msg 'push!) (set! stack (append (reverse args) stack))]
+        [(eq? msg 'stack) stack]
+        [else "Not valid message!"]))))
+
+
 (define per (remove-duplicates (permutations start)))
 
 (define nums( list 100 50 25 10 2 1))
