7ea4376c9e48c748ad9e19b1ac7853a7803bb4d2
dstarr86 <starrdara@gmail.com>
2017-04-24 02:34:25 +0100

    checking to see is calculate rpn and valid-rpn are working

--- Countdown.rkt
+++ Countdown.rkt
@@ -3,35 +3,44 @@
 ;==========================================================================================
 ;Used Originally to try work out solution:
 
-;defining a list with all possible numbers
+;defining a list with all possible numbers to games spec
 (define ttllist (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
-
-(define oprs(list '+ '- '* '/))
-
+(define oprs (list '+ '- '/ '*))
 
 ; Append list of numbers and operators
 (define full-list (append ttllist oprs))
+(display full-list)
+(display "\n")
+
 ;defining a list of set number and all possible permutations = 720
 (define setnums( list 100 50 25 10 2 1))
-(permutations setnums)
+(define allnums (remove-duplicates (permutations setnums)))
+;see all number permutations without duplicates
+;(display allnums)
+
+
 ;defining operators and all possibilities of 5
-(define operators(list '+ '- '* '/))
-(cartesian-product operators operators operators operators operators)
+(define op(list '+ '- '* '/))
+(define allops(cartesian-product op op op op op))
+;(display allops)
 
-(cartesian-product setnums setnums operators)
 
-
+;display all numbers and all operators
+;commented out because does not need to be ran
+;(cartesian-product allnums allops)
 
 ; A randomised target number
 (define rand (random 101 999))
 (display rand)
+(display "\n")
 
 
 ;===========================================================================================
 ;Solution Attempt From here
 
+;empty list nums
 (define nums null)
-
+;all operators
 (define ops(list '+ '+ '+ '+ '+ '- '- '- '- '- '- '* '* '* '* '*  '/ '/ '/ '/ '/))
 
 
@@ -41,19 +50,29 @@
   (for/fold ([stack '()]) ([token expr])
     ;(printf "~a\t -> ~a~N" token stack) ; Uncomment to see workings, not recommended for long lists.
     (match* (token stack)
-     [((? number? n) stk) (cons n stk)]
-     [('+ (list x y stk ___)) (cons (+ x y) stk)]
-     [('- (list x y stk ___)) (cons (- y x) stk)]
-     [('* (list x y stk ___)) (cons (* x y) stk)]
-     [('/ (list x y stk ___)) (if (= y 0)
-                                (cons 0 stk)
+     [((? number? n) s) (cons n s)]
+     [('+ (list x y s ___)) (cons (+ x y) s)]
+     [('- (list x y s ___)) (cons (- y x) s)]
+     [('* (list x y s ___)) (cons (* x y) s)]
+     [('/ (list x y s ___)) (if (= y 0)
+                                (cons 0 s)
                                 (if (= x 0)
-                                    (cons 0 stk)
-                                    (cons (/ x y) stk)))]
+                                    (cons 0 s)
+                                    (cons (/ x y) s)))]
      [(x stk) (error "calculate-RPN: Cannot calculate the expression:" 
                    (reverse (cons x stk)))])))
- 
-;creating a stack
+
+;stack stk starting at 0
+;Pass expression
+;If next element is number increase stk by 1
+;If next element is operand check if stk > 1
+;If stk > 1 decrease r by 1 and move on
+;if stk < 2 expression is not rpn-compatible
+
+;check if rpn is workin this should = 48
+;uncomment to check
+;(calculate-RPN '(4 6 8 + 2 - * ))
+
 (define (valid-rpn? e[stk 0])
   (if(null? e)
      (if (= stk 1)
@@ -64,7 +83,7 @@
         (if(> stk 1)
            (valid-rpn? (cdr e) (- stk 1))
            #f))))
-
+(valid-rpn?  (list 25 50 * 6 1 * 5 - 2 3 * +))
 
 
 
